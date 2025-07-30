ec42350b22625550aaeab19541901f7e0b18837f
Kumar Mayank <kumarmayank2090@gmail.com>
2025-07-30 08:33:36 +0000

    Variables

---
+++ Basics.js/variables.js
@@ -0,0 +1,12 @@
+ //let ,const,var 
+
+ /*notes-: prefer not to use var
+ because of issue in block  scope and function scope
+ */
+let accountEmail ="kumar@google.com"
+const accountId=12345445
+var accountPassword ="12345"
+accountCity="jaipur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
