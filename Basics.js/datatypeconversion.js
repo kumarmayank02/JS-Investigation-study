76e7537477839e1b58fecade0388a8f92e178b47
Kumar Mayank <kumarmayank2090@gmail.com>
2025-07-30 08:41:40 +0000

    DataTypesConversions

---
+++ Basics.js/datatypeconversion.js
@@ -0,0 +1,27 @@
+// it is very vast but very important 
+let score=true
+// console.log(typeof score)
+// console.log(typeof (score))
+let valuInNymver=Number(score)
+// console.log(typeof valuInNymver )
+// console.log(valuInNymver)
+
+//"33"=> 33
+//"33abc" =>Nan
+//true=>1 false=>0
+
+ let isLoggedIn= ""
+ let booleanIsLoggedIn=Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+// 1 =>boolean 0=>false
+// ""=> false
+//"mayank"=> true 
+
+//for string
+let  someNumber=33
+let strinNumber =String(someNumber)
+console.log(strinNumber)
+console.log(typeof strinNumber)
+
+// *******operation***********
+
