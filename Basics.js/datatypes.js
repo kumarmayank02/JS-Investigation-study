8b70cfc376157f4d46caf6f4fad4e53f6bbfa6db
Kumar Mayank <kumarmayank2090@gmail.com>
2025-07-30 08:39:05 +0000

    DataTypes

---
+++ Basics.js/datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all js code as newer version
+//alert(3 +3 )//statements(now we are using node js)
+
+// console.log(3 + 3) (code readability)
+// console.log("kumar mayank");
+
+let name ="mayank"
+let age = 19
+let isLoggedIn=false
+
+//number => 2 to power 53
+// bignit
+//string =>""
+//boolean => true/false
+//null =>standalone value
+//undefined =>
+//symbol => unique 
+
+//object 
+
+console.log(typeof "")//string type
+console.log(typeof null)//object type
+console.log(typeof undefined)//undefined
