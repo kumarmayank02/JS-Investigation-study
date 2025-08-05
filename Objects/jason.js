c9694ce31912ebf3916dc9660dd3700a6df7034f
Kumar Mayank <kumarmayank2090@gmail.com>
2025-08-05 07:38:23 +0000

    object destructure

---
+++ Objects/jason.js
@@ -0,0 +1,33 @@
+//Object Destructure and Json Api Introdduction
+const course = {
+    coursename:"I am study js ",
+    price:"999",
+    courseInstructor:"Mayank"
+
+}
+//course.courseInstructor
+
+const {price,coursename,courseInstructor:instructor}=course//here i have do de structure of object
+console.log(instructor);
+console.log(price);
+console.log(coursename);
+
+//******Api***
+// //{
+//      "name":"Kumar Mayank",
+//      "coursename": "javaScript",
+//      "price":"free"
+
+// }
+
+[
+    {
+
+    },
+    {
+
+    },
+    {
+
+    }
+]
