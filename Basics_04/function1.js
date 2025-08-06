35f6fcda891622689a9c99d6ab866d81ba9b0dee
Kumar Mayank <kumarmayank2090@gmail.com>
2025-08-06 09:21:29 +0000

    function1

---
+++ Basics_04/function1.js
@@ -0,0 +1,48 @@
+// console.log("M");
+// console.log("0");
+// console.log("N");
+// console.log("D");
+// console.log("A");
+// console.log("Y");
+
+// function firstDay(){
+// console.log("M");
+// console.log("0");
+// console.log("N");
+// console.log("D");
+// console.log("A");
+// console.log("Y");
+// }
+
+// firstDay()
+
+//function addThreeNumbers(number1,number2,number3) it is called parameters
+// function addThreeNumbers(number1,number2,number3){
+//     console.log(number1 + number2 + number3);
+    
+// }
+// function addThreeNumbers(number1,number2,number3){
+//    let result =number1 + number2 + number3
+//     console.log("Mayank");
+    // return result
+    // return number1 + number2 + number3
+   
+// }
+// addThreeNumbers(2,5,80)
+// //addThreeNumbers(2,5,80) its called arguments
+// const result = addThreeNumbers(2,5,80)
+// console.log("Result:",result);
+
+
+function loginUser(username = "Minz"){
+    if(!username){
+console.log("Please enter a user name");
+ return 
+    }
+    return `${username} just logged in`
+}
+// console.log(loginUser("Mayank"))
+console.log(loginUser("Mayank"))
+
+
+
