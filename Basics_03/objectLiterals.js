f0bf03cef0ed96fde86d2df37ef576a54e205cfe
Kumar Mayank <kumarmayank2090@gmail.com>
2025-08-07 07:04:34 +0000

    literal in objects

---
+++ Basics_03/objectLiterals.js
@@ -0,0 +1,27 @@
+const student ={
+    name:"Kumar Mayank",
+    age:23,
+    degree:"MCA",
+    sem:"Three",
+    marks:90,
+    city:"Ranchi",
+    array:["mayank","chiku",7,8] 
+
+}
+console.log(student);
+
+let delhi ={
+    name:"Ajay",
+    age:30
+}
+console.log(delhi)
+//create an objects literal for the property of thread /twitter posst whicch includes username,content ,likes, report,tags
+let post ={
+    username:"Kumar Mayank",
+    content:"youtube Viral video",
+    likes:500,
+    reports:5,
+    tags:["java language","Python Programming"]
+
+}
+console.log(post)
