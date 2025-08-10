484411e8e743fd913edecff128e5a162e176869c
Kumar Mayank <kumarmayank2090@gmail.com>
2025-08-10 17:51:12 +0000

    example to create table   by functiion

---
+++ Basics_04/functionQuestion1imp.js
@@ -0,0 +1,12 @@
+//Create  a function that print the multiple table of a  number
+function printTable(n){
+    for(let i=n; i<= n*10 ; i+=n)
+    {
+        console.log(i);
+    }
+}
+printTable(4)
+ //it will print the table 
+
+ //it will print the result 4 table in output
+ 
